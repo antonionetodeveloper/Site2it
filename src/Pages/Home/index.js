f113360d69e01c483b42f61131abd35b6e87b496
Antônio <antonio.fernandes.de.santana.neto@gmail.com>
2022-01-17 18:24:51 -0300

    alter banner image for mobile

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,156 +1,173 @@
 import { Link } from "react-router-dom"
 
+
 import "./styles.css"
 
 import logo from "../../Images/assets/logo-png.png"
 import menu from "../../Images/assets/menu.png"
 import back from "../../Images/assets/back.png"
 
-import woman from "../../Images/pages/index/woman.png"
+import womanLight from "../../Images/pages/index/woman.png"
+import womanNoLight from "../../Images/pages/index/womanNoLight.png"
 import money from "../../Images/pages/index/money.jpg"
 import xp from "../../Images/pages/index/xp.jpg"
 
+
+
 export default function Home() {
+  
+  
+  let woman = ""
+  function setWomanImage(){
 
-    return(
-        <div className="AppHome">
-            <header>
-          <div className="header">
-            <div id="mobile_menu">
-            
-              <nav id="nav">
-                <button id="btn_menu">
-                    <img alt="" src={menu} id="img_menu"/>
-                </button>
-                <button id="btn_fecha">
-                    <img alt="" src={back} id="img_fecha"/>
-                </button>
-                
-                <ul id="menu">
-                  <li>
-                    <a rel="noreferrer" href="CComponents/Site2it/Contents/Prices/prices.html">
-                      Exemplos
-                    </a>
-                  </li>
+    if(window.innerWidth <= 900){
+      woman = womanNoLight
+    }if(window.innerWidth > 900){
+      woman = womanLight
+    }
+  }setWomanImage()
 
-                  <li>
-                    <a rel="noreferrer" href="CComponents/Site2it/Contents/Prices/prices.html">
-                      Valores
-                    </a>
-                  </li>
 
-                  <li>
-                    <a rel="noreferrer" href="CComponents/Site2it/Contents/Prices/prices.html">
-                      Já sou cliente
-                    </a>
-                  </li>
 
-                  <li>
-                    <a rel="noreferrer" href="CComponents/Site2it/Contents/Prices/prices.html">
-                      Contato
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-          </div>
+  return(
+      <div className="AppHome">
+          <header>
+        <div className="header">
+          <div id="mobile_menu">
+          
+            <nav id="nav">
+              <button id="btn_menu">
+                  <img alt="" src={menu} id="img_menu"/>
+              </button>
+              <button id="btn_fecha">
+                  <img alt="" src={back} id="img_fecha"/>
+              </button>
               
-          <div id="nao_menu">
-            <div className="logo">
-                <a rel="noreferrer" href="https://site2it.com">
-                  <img alt="" src={logo} className="img_logo"/>
-                  <h2>Site2it</h2>
-                </a>
-            </div>
-                  
-            <div className='img_e_txt'>
-              <div className="slogan_header">
-                <h1 className="slogan">
-                  GARANTA JÁ<br/>
-                  O SEU SITE
-                </h1>
-                  <Link to="/atendimento">
-                    <button id="fillButtonEffect1" className="btn">
-                      EU QUERO
-                    </button>
-                  </Link>
-              </div>
+              <ul id="menu">
+                <li>
+                  <a rel="noreferrer" href="CComponents/Site2it/Contents/Prices/prices.html">
+                    Exemplos
+                  </a>
+                </li>
 
-                <div className="woman">
-                  <img alt="" src={woman} id="woman"/>
-                </div>
+                <li>
+                  <a rel="noreferrer" href="CComponents/Site2it/Contents/Prices/prices.html">
+                    Valores
+                  </a>
+                </li>
+
+                <li>
+                  <a rel="noreferrer" href="CComponents/Site2it/Contents/Prices/prices.html">
+                    Já sou cliente
+                  </a>
+                </li>
+
+                <li>
+                  <a rel="noreferrer" href="CComponents/Site2it/Contents/Prices/prices.html">
+                    Contato
+                  </a>
+                </li>
+              </ul>
+            </nav>
+        </div>
+            
+        <div id="nao_menu">
+          <div className="logo">
+              <a rel="noreferrer" href="https://site2it.com">
+                <img alt="" src={logo} className="img_logo"/>
+                <h2>Site2it</h2>
+              </a>
+          </div>
+                
+          <div className='img_e_txt'>
+            <div className="slogan_header">
+              <h1 className="slogan">
+                GARANTA JÁ<br/>
+                O SEU SITE
+              </h1>
+                <Link to="/atendimento">
+                  <button id="fillButtonEffect1" className="btn">
+                    EU QUERO
+                  </button>
+                </Link>
+            </div>
+
+              <div className="woman">
+                <img alt="" src={woman} id="woman"/>
               </div>
             </div>
           </div>
-        </header>
-    
-    
-    
-        <main>
-          <section id="section1">
-            <div id="xp">
-              <div className="img">
-                <img alt="" src={xp} id="img_xp"/>
-              </div>
-                
-              <div className="txt_xp">
+        </div>
+      </header>
+  
+  
+  
+      <main>
+        <section id="section1">
+          <div id="xp">
+            <div className="img">
+              <img alt="" src={xp} id="img_xp"/>
+            </div>
+              
+            <div className="txt_xp">
+              <h3>
+                Há mais de<br/>
+                2 anos no<br/>
+                mercado
+              </h3>
+              
+              <p>
+                Entregando sites de<br/>
+                qualidade máxima
+              </p>
+              
+              <Link to="/exemplos">
+                <button id="bigButtonEffect" className="exemplos">
+                  VER EXEMPLOS
+                </button>
+              </Link>
+            </div>
+              
+          </div>
+        </section>
+      
+      
+        <section id="section2">
+          <div id="money">
+            <div className="txt_e_btn">
+              <div className="txt_money">
                 <h3>
-                  Há mais de<br/>
-                  2 anos no<br/>
-                  mercado
+                  O PREÇO MAIS<br/>
+                  ACESSÍVEL DE<br/>
+                  TODOS
                 </h3>
                 
                 <p>
-                  Entregando sites de<br/>
-                  qualidade máxima
+                  Temos um site para<br/>
+                  você com suas<br/>
+                  necessidades
                 </p>
-                
-                <Link to="/exemplos">
-                  <button id="bigButtonEffect" className="exemplos">
-                    VER EXEMPLOS
+              </div>
+                  
+              <Link to="/valores">
+                  <button id="fillButtonEffect2" className="valores">
+                    VER VALORES
                   </button>
                 </Link>
-              </div>
-                
+              
             </div>
-          </section>
-        
-        
-          <section id="section2">
-            <div id="money">
-              <div className="txt_e_btn">
-                <div className="txt_money">
-                  <h3>
-                    O PREÇO MAIS<br/>
-                    ACESSÍVEL DE<br/>
-                    TODOS
-                  </h3>
-                  
-                  <p>
-                    Temos um site para<br/>
-                    você com suas<br/>
-                    necessidades
-                  </p>
-                </div>
-                    
-                <Link to="/valores">
-                    <button id="fillButtonEffect2" className="valores">
-                      VER VALORES
-                    </button>
-                  </Link>
-                
-              </div>
-                
-              <div className="img">
-                <img alt="" src={money} id="money"/>
-              </div>
+              
+            <div className="img">
+              <img alt="" src={money} id="money"/>
             </div>
-          </section>
-        </main>
-    
-    
-        <footer>
-          <h5>Feito por: Site2it</h5>
-        </footer>
-      </div>
-    )
+          </div>
+        </section>
+      </main>
+  
+  
+      <footer>
+        <h5>Feito por: Site2it</h5>
+      </footer>
+    </div>
+  )
 }
